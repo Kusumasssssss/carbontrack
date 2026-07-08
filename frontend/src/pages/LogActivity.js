70533e76fa966f843b2376e3db44446a98c0ee65
Kusumasssssss <kusumas5132@gmail.com>
2026-07-08 18:06:35 +0530

    Resolve App.js merge conflict

--- frontend/src/pages/LogActivity.js
+++ frontend/src/pages/LogActivity.js
@@ -67,7 +67,11 @@ function LogActivity() {
       });
 
       if (!response.ok) {
-        throw new Error("Failed");
+        const errorText = await response.text();
+        console.log("Status:", response.status);
+        console.log("Error:", errorText);
+        alert(errorText);
+        return;
       }
 
       alert(
